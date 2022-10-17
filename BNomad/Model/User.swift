fc6ae03d3e0875c952928576ba061b0297ff10b1
sunshiningsoo <yenkee1219@gmail.com>
2022-10-17 11:50:21 +0900

    [Feat] User 객체의 isChecked 속성을 computed property로 변환

--- BNomad/Model/User.swift
+++ BNomad/Model/User.swift
@@ -10,7 +10,8 @@ import Foundation
 struct User {
     let userUid: String
     var nickname: String
-    var isChecked: Bool
     var placeUid: String?
     var occupation: String?
+
+    var isChecked: Bool { placeUid != nil ? true : false }
 }

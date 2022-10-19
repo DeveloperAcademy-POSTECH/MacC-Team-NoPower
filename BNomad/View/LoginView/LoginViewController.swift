a89768315328f5a116ff36225a093c7bc32466d0
Rain <eunbkang@gmail.com>
2022-10-19 14:42:49 +0900

    [Feat] #15 Sign in with Apple 추가, 애플로그인 버튼 표시

--- BNomad/View/LoginView/LoginViewController.swift
+++ BNomad/View/LoginView/LoginViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 박성수 on 2022/10/17.
 //
 
+import AuthenticationServices
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -18,28 +19,49 @@ class LoginViewController: UIViewController {
         return view
     }()
     
+    private let loginButton: ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton(type: .signIn, style: .whiteOutline)
+        button.cornerRadius = 50
+        
+        return button
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
-        configLogo()
+        
+        configUI()
+        setupProviderLoginView()
     }
     
     // MARK: - Methods
     
-    func configLogo() {
+    func setupProviderLoginView() {
+        loginButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+    }
+    
+    func configUI() {
         let viewWidth = view.bounds.width
         let viewHeight = view.bounds.height
         let imageWidth = viewWidth * 196/390
         let imageHeight = viewHeight * 33/844
+        let buttonWidth = viewWidth * 268/390
+        let buttonHeight = viewHeight * 50/844
+        let buttonBottom = viewHeight * 143/844
+        
+        view.backgroundColor = .white
         
         view.addSubview(logoView)
-        logoView.translatesAutoresizingMaskIntoConstraints = false
-        logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        logoView.widthAnchor.constraint(equalToConstant: imageWidth).isActive = true
-        logoView.heightAnchor.constraint(equalToConstant: imageHeight).isActive = true
+        logoView.center(inView: view)
+        logoView.setDimensions(height: imageHeight, width: imageWidth)
+        
+        view.addSubview(loginButton)
+        loginButton.centerX(inView: logoView)
+        loginButton.anchor(bottom: view.bottomAnchor, paddingBottom: buttonBottom, width: buttonWidth, height: buttonHeight)
+    }
+    
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        print(#function)
     }
 }

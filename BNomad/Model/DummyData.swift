8e3669fd394a1dcdfe8d90d9f7229f7a60d4ac42
limhyoseok <nonamedoud@gmail.com>
2022-10-22 11:23:26 +0900

    [Feat]  사용하지 않는 position 더미데이터 제거

--- BNomad/Model/DummyData.swift
+++ BNomad/Model/DummyData.swift
@@ -17,7 +17,4 @@ struct DummyData {
     static var place2: Place = Place(placeUid: UUID().uuidString, name: "애플디벨로퍼아카데미", latitude: 36.01411, longitude: 129.32587, contact: "054-279-0114")
     static var place3: Place = Place(placeUid: UUID().uuidString, name: "포스빌", latitude: 36.01511, longitude: 129.34587, contact: "054-279-0115")
     
-    static var poistion1: Position = Position(id: user1.userUid, latitude: 36.0128, longitude: 129.3265)
-    static var poistion2: Position = Position(id: user2.userUid, latitude: 36.0152, longitude: 129.3465)
-    static var poistion3: Position = Position(id: user3.userUid, latitude: 36.1248, longitude: 129.5265)
 }

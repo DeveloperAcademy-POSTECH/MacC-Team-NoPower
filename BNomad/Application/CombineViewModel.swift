6e37d95fa4f5fedd2a1c3cd5823f731973607900
limhyoseok <nonamedoud@gmail.com>
2022-10-25 20:50:56 +0900

    [feat] viewModel static으로 변경, isLogIn computed property로 변경

--- BNomad/Application/CombineViewModel.swift
+++ BNomad/Application/CombineViewModel.swift
@@ -10,9 +10,10 @@ import Combine
 
 class CombineViewModel: ObservableObject {
     
-    static let shared = CombineViewModel()
+    static let shared: CombineViewModel = CombineViewModel()
     
     @Published var user: User?
     @Published var placeInCurrentMap: [Place] = [DummyData.place1, DummyData.place1, DummyData.place2] //
-    var isLogIn = false
+    
+    var isLogIn: Bool { user != nil }
 }

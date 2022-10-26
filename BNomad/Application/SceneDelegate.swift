686cbe96c67f1d15cff555f6ca70551dda40c175
lululll <yeekim182@gmail.com>
2022-10-27 02:19:08 +0900

    [Feat] SceneDelegate 원복

--- BNomad/Application/SceneDelegate.swift
+++ BNomad/Application/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
-//        window?.rootViewController = PlaceCheckInViewController()
         window?.makeKeyAndVisible()
     }
 

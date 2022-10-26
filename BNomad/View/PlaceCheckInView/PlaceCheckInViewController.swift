da67c9b0441c6a9b119ad7048117b32addfd0ac6
lululll <yeekim182@gmail.com>
2022-10-27 02:37:33 +0900

    [Feat] 코멘트 반영

--- BNomad/View/PlaceCheckInView/PlaceCheckInViewController.swift
+++ BNomad/View/PlaceCheckInView/PlaceCheckInViewController.swift
@@ -13,10 +13,7 @@ class PlaceCheckInViewController: UIViewController {
     var tmpUserUid = "04d3acd1-a6ec-465e-845e-a319e42180e6"
     let placeUid = "49ab61cf-f05f-45b7-9168-8ab58983620c"
     
-    var selectedPlace: Place? {
-        didSet {
-        }
-    }
+    var selectedPlace: Place?
     
     // MARK: - Properties
     

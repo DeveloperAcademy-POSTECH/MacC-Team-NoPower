cb1279de656c6e175c819ffb3d270a4347a0e6db
jinwp <jinwoong0925@gmail.com>
2022-11-11 15:55:59 +0900

    [Feat] #169 ReveiwDetailViewController UI 작업

--- BNomad/View/ReviewView/ReviewDetailViewController.swift
+++ BNomad/View/ReviewView/ReviewDetailViewController.swift
@@ -9,12 +9,6 @@ import UIKit
 import AVFoundation
 
 class ReviewDetailViewController: UIViewController {
-
-    var counter: Int = 1 {
-        didSet {
-            self.collectionView.reloadData()
-        }
-    }
     
     private lazy var dismissButton: UIButton = {
         let button = UIButton()
@@ -49,13 +43,11 @@ class ReviewDetailViewController: UIViewController {
     
     lazy var reviewTextView: UITextView = {
         let textview = UITextView()
-//        textview.textInputView.backgroundColor = .white
         textview.font = .preferredFont(forTextStyle: .body, weight: .regular)
         textview.textColor = .black
         textview.layer.borderWidth = 1
         textview.layer.borderColor = CustomColor.nomadBlue?.cgColor
         textview.layer.cornerRadius = 8
-//        textview.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
         textview.textContainerInset = .init(top: 14, left: 14, bottom: 14, right: 14)
         textview.scrollIndicatorInsets = .init(top: 10, left: 10, bottom: 10, right: 10)
         textview.text = "리뷰를 입력해주세요."
@@ -83,10 +75,57 @@ class ReviewDetailViewController: UIViewController {
         button.backgroundColor = .white
         button.layer.borderWidth = 1
         button.layer.borderColor = CustomColor.nomadBlue?.cgColor
-//        button.layer.opacity = 0.5
         button.addTarget(self, action: #selector(chooseCameraOrAlbum), for: .touchUpInside)
         return button
     }()
+    
+    private var addPhotoCell: UIButton = {
+        let button = UIButton()
+        let config = UIImage.SymbolConfiguration(pointSize: 15)
+        button.setImage(UIImage(systemName: "plus", withConfiguration: config), for: .normal)
+        button.layer.cornerRadius = 8
+        button.tintColor = UIColor(hex: "3C3C43")?.withAlphaComponent(0.6)
+        button.backgroundColor = UIColor(hex: "F5F5F5")
+        button.addTarget(self, action: #selector(chooseCameraOrAlbum), for: .touchUpInside)
+//        button.anchor(width: (UIScreen.main.bounds.width-64)/3, height: (UIScreen.main.bounds.width-64)/3)
+        return button
+    }()
+    
+    private lazy var removePhotoCell: UIImageView = {
+        var image = UIImage(named: "AppIcon")
+        var imageView = UIImageView(image: image)
+        imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
+        imageView.isHidden = true
+        imageView.isUserInteractionEnabled = true
+        imageView.addSubview(removeButton)
+        removeButton.anchor(top: imageView.topAnchor, right: imageView.rightAnchor, paddingTop: 0, paddingRight: 0, width: 25, height: 25)
+        return imageView
+    }()
+    
+    private var removeButton: UIButton = {
+        let button = UIButton()
+        let config = UIImage.SymbolConfiguration(pointSize: 14)
+        button.setImage(UIImage(systemName: "xmark", withConfiguration: config), for: .normal)
+        button.tintColor = .white
+        button.layer.backgroundColor = UIColor.black.withAlphaComponent(0.7).cgColor
+        button.addTarget(self, action: #selector(removePhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func removePhoto() {
+        let actionsheet =  UIAlertController(title: "사진을 지우시겠습니까?", message: nil, preferredStyle: .alert)
+        let no = UIAlertAction(title: "취소", style: .cancel) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        let yes = UIAlertAction(title: "확인", style: .default) { action in
+            self.addPhotoCell.isHidden = false
+            self.removePhotoCell.isHidden = true
+        }
+        actionsheet.addAction(no)
+        actionsheet.addAction(yes)
+        present(actionsheet, animated: true, completion: nil)
+    }
     
     @objc func chooseCameraOrAlbum() {
         let actionsheet =  UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -108,8 +147,7 @@ class ReviewDetailViewController: UIViewController {
         camera.sourceType = .camera
         camera.allowsEditing = true
         camera.cameraDevice = .rear
-//        camera.cameraCaptureMode = .photo
-//        camera.delegate = self
+        camera.delegate = self
         present(camera, animated: true, completion: nil)
     }
     
@@ -117,6 +155,7 @@ class ReviewDetailViewController: UIViewController {
         let album = UIImagePickerController()
         album.sourceType = .photoLibrary
         album.allowsEditing = true
+        album.delegate = self
         present(album, animated: true, completion: nil)
     }
 
@@ -127,38 +166,11 @@ class ReviewDetailViewController: UIViewController {
         button.titleLabel?.textColor = .white
         button.layer.cornerRadius = 8
         button.backgroundColor = CustomColor.nomadBlue
-//        button.addTarget(self, action: #selector(chooseCameraOrAlbum), for: .touchUpInside)
+//        button.addTarget(self, action: #selector(), for: .touchUpInside)
         return button
     }()
     
-    private let layout: UICollectionViewFlowLayout = {
-        let guideline = UICollectionViewFlowLayout()
-        guideline.scrollDirection = .vertical
-        guideline.minimumLineSpacing = 9
-        guideline.minimumInteritemSpacing = 0
-        return guideline
-    }()
-
-    private lazy var collectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
-        view.isScrollEnabled = true
-        view.showsHorizontalScrollIndicator = false
-        view.showsVerticalScrollIndicator = true
-        view.scrollIndicatorInsets = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 4)
-        view.contentInset = .zero
-        view.backgroundColor = .white
-        view.clipsToBounds = true
-        view.dataSource = self
-        view.delegate = self
-        view.register(AddPhotoCell.self, forCellWithReuseIdentifier: AddPhotoCell.identifier)
-        view.register(RemovePhotoCell.self, forCellWithReuseIdentifier: RemovePhotoCell.identifier)
-        return view
-    }()
-        
-    // MARK: - LifeCycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private func configureUI() {
         view.backgroundColor = .white
         view.addSubview(dismissButton)
         dismissButton.anchor(top: view.topAnchor, right: view.rightAnchor, paddingTop: 66, paddingRight: 17, width: 30, height: 30)
@@ -170,13 +182,24 @@ class ReviewDetailViewController: UIViewController {
         reviewTextView.anchor(top: shareExperienceText.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 28, paddingLeft: 20, paddingRight: 20, height: 207)
         view.addSubview(addPhotoText)
         addPhotoText.anchor(top: reviewTextView.bottomAnchor, left: view.leftAnchor, paddingTop: 60, paddingLeft: 26)
-        view.addSubview(collectionView)
-        collectionView.anchor(top: addPhotoText.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 95, paddingRight: 20)
+        view.addSubview(addPhotoCell)
+        addPhotoCell.anchor(top: addPhotoText.bottomAnchor, left: view.leftAnchor, paddingTop: 10, paddingLeft: 20, width: 110, height: 110)
+        view.addSubview(removePhotoCell)
+        removePhotoCell.anchor(top: addPhotoText.bottomAnchor, left: view.leftAnchor, paddingTop: 10, paddingLeft: 20, width: 110, height: 110)
         view.addSubview(checkButton)
         checkButton.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 20, paddingBottom: 33, paddingRight: 20, height: 46)
     }
 
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
 }
+
+// MARK: - UITextViewDelegate
 
 extension ReviewDetailViewController: UITextViewDelegate {
     
@@ -188,7 +211,7 @@ extension ReviewDetailViewController: UITextViewDelegate {
     }
 
     func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+        if textView.text.isEmpty {
             textView.text = "리뷰를 입력해주세요."
             textView.textColor = CustomColor.nomadGray1
         }
@@ -199,58 +222,14 @@ extension ReviewDetailViewController: UITextViewDelegate {
 // MARK: - UIImagePickerControllerDelegate
 
 extension ReviewDetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-}
-
-// MARK: - UICollectionViewDataSource
-
-extension ReviewDetailViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.counter
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell
-        guard let addingCell = collectionView.dequeueReusableCell(withReuseIdentifier: AddPhotoCell.identifier, for: indexPath) as? AddPhotoCell else { return UICollectionViewCell() }
-        guard let removingCell = collectionView.dequeueReusableCell(withReuseIdentifier: RemovePhotoCell.identifier, for: indexPath) as? RemovePhotoCell else { return UICollectionViewCell() }
-        if indexPath.item == self.counter - 1 {
-            cell = addingCell
-        } else {
-            cell = removingCell
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                chooseCameraOrAlbum()
-                self.counter += 1
-            }
-            dismiss(animated: true)
-        }
-        
-        return cell
-    }
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-}
-
-// MARK: - UICollectionViewDelegate
-
-extension ReviewDetailViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        chooseCameraOrAlbum()
-        self.counter += 1
-    }
-}
-
-// MARK: - UICollectionViewDelegateFlowLayout
-
-extension ReviewDetailViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (UIScreen.main.bounds.width-64)/3, height: (UIScreen.main.bounds.width-64)/3)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            removePhotoCell.image = image
+            addPhotoCell.isHidden = true
+            removePhotoCell.isHidden = false
+        }
+        dismiss(animated: true, completion: nil)
     }
     
 }

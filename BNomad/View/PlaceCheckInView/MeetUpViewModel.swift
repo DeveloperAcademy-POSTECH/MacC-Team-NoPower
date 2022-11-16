073b54b83c1fcd3fb6390b206dcdcb401bbf257d
limhyoseok <nonamedoud@gmail.com>
2022-11-16 11:02:47 +0900

    [Feat] meetUpViewModel 추가

---
+++ BNomad/View/PlaceCheckInView/MeetUpViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MeetUpViewModel.swift
+//  BNomad
+//
+//  Created by hyo on 2022/11/14.
+//
+
+import Foundation
+
+class MeetUpViewModel: ObservableObject {
+    
+    @Published var meetUp: MeetUp?
+    
+}

683974ec3eb27b89a75cfac7cd0b80d03a0ec2cb
jinwp <jinwoong0925@gmail.com>
2022-11-18 18:06:55 +0900

    [Feat] #282 ReviewListView Data Binding

--- BNomad/View/ReviewView/ReviewCell.swift
+++ BNomad/View/ReviewView/ReviewCell.swift
@@ -6,29 +6,34 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ReviewCell: UICollectionViewCell {
     
     static let identifier = "ReviewCell"
 
     // MARK: - Properties
-        
-    lazy var cell: UIView = {
-        let view = UIView()
-        view.clipsToBounds = true
-        view.backgroundColor = .white
-        view.addSubview(divider)
-        view.addSubview(review)
-        view.addSubview(userImage)
-        view.addSubview(userName)
-
-        divider.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, height: 0.5)
-        review.anchor(top: divider.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12, paddingLeft: 21, paddingRight: 22)
-        userImage.anchor(top: review.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, paddingTop: 4, paddingLeft: 21, paddingBottom: 11,  width: 20, height: 20)
-        userName.centerY(inView: userImage, leftAnchor: userImage.rightAnchor, paddingLeft: 8)
-
-        return view
-    }()
+    
+    var review: Review? {
+        didSet {
+            guard let review = review else { return }
+            self.reviewText.text = review.content
+            if let imageString = review.imageUrl {
+                self.photo.isHidden = false
+                self.photo.anchor(top: divider.bottomAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 12, paddingLeft: 20, paddingRight: 20, height: 174)
+                self.photo.kf.setImage(with: URL(string: imageString))
+            } else {
+                self.photo.isHidden = true
+                self.reviewText.anchor(top: self.divider.bottomAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 12, paddingLeft: 21, paddingRight: 22)
+            }
+            FirebaseManager.shared.fetchUser(id: review.userUid) { user in
+                self.userName.text = user.nickname
+                if let userImageUrl = user.profileImageUrl {
+                    self.userImage.kf.setImage(with: URL(string: userImageUrl))
+                }
+            }
+        }
+    }
     
     var divider: UIView = {
         let rectangle = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
@@ -39,18 +44,20 @@ class ReviewCell: UICollectionViewCell {
     var photo: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 8
         return imageView
     }()
     
-    lazy var review: UILabel = {
+    var reviewText: UILabel = {
         let text = UILabel()
         text.backgroundColor = .clear
         text.textColor = .black
         text.font = .preferredFont(forTextStyle: .subheadline, weight: .regular)
         text.numberOfLines = 0
         text.lineBreakMode = .byWordWrapping
-        text.text = "한줄 소개 입니다. 오늘 막거리 고 하나요. 어제는 집콕하며 여유를 즐겼는데~~ 오늘은 밖에서 뷰를 그리려니 심심합니다. 뭐가 난이도가 있다는 걸까요? 결국은 알아내는 사람이더라고요. WILL ZZANGZZANG 읏추 너무 춥습니다. 알쏭달쏭한 겨울입니다. 텍스트 길이제한이 없어서 아무말이나 적을 수 있답니다. 그래도 맥시멈을 설정해야할까요? 그렇게 많은 리뷰를 남길 것 같진 않은데.. 뭐 일단 해보죠!! 해봅시다 <3"
+        text.text = ""
         text.textAlignment = .left
         return text
     }()
@@ -58,7 +65,8 @@ class ReviewCell: UICollectionViewCell {
     private var userImage: UIImageView = {
         let image = UIImage(named: "AppIcon")
         let imageView = UIImageView(image: image)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = imageView.bounds.width/2
+        imageView.clipsToBounds = true
         return imageView
     }()
     
@@ -80,13 +88,21 @@ class ReviewCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpCell()
+        self.clipsToBounds = true
+        self.anchor(top: contentView.topAnchor, width: UIScreen.main.bounds.width)
+        self.backgroundColor = .white
+        self.addSubview(divider)
+        self.addSubview(reviewText)
+        self.addSubview(userImage)
+        self.addSubview(userName)
+
+        self.addSubview(photo)
+
+        divider.anchor(top: self.topAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, height: 0.5)
+        userImage.anchor(top: reviewText.bottomAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, paddingTop: 4, paddingLeft: 21, paddingBottom: 11, width: 20, height: 20)
+        userName.centerY(inView: userImage, leftAnchor: userImage.rightAnchor, paddingLeft: 8)
+        photo.anchor(top: divider.bottomAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 12, paddingLeft: 20, paddingRight: 20, height: 174)
+        reviewText.anchor(top: photo.bottomAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 8, paddingLeft: 21, paddingRight: 22)
     }
 
-    // MARK: - Helpers
-    
-    func setUpCell() {
-        self.addSubview(cell)
-        cell.anchor(top: self.topAnchor, width: UIScreen.main.bounds.width)
-    }
 }

7dece7476df73fdfeca09ac2c7cf9fdacdec769c
sunshiningsoo <yenkee1219@gmail.com>
2022-11-22 16:32:28 +0900

    [Bug] #291 locationCheck MapViewController에 재삽입

--- BNomad/View/MapView/MapViewController.swift
+++ BNomad/View/MapView/MapViewController.swift
@@ -490,6 +490,27 @@ class MapViewController: UIViewController {
             }
             .store(in: &store)
     }
+    
+    func locationCheck(){
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == CLAuthorizationStatus.denied || status == CLAuthorizationStatus.restricted {
+            let alter = UIAlertController(title: "위치 접근 허용 설정이 제한되어 있습니다.", message: "해당 장소의 장소보기 및 체크인 기능을 사용하려면 위치 접근을 허용해주셔야 합니다. 앱 설정 화면으로 가시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+            let logOkAction = UIAlertAction(title: "설정", style: UIAlertAction.Style.default){
+                (action: UIAlertAction) in
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(NSURL(string:UIApplication.openSettingsURLString)! as URL)
+                } else {
+                    UIApplication.shared.openURL(NSURL(string: UIApplication.openSettingsURLString)! as URL)
+                }
+            }
+            let logNoAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.destructive)
+            alter.addAction(logNoAction)
+            alter.addAction(logOkAction)
+            self.present(alter, animated: true, completion: nil)
+        }
+    }
+    
 }
 
 // MARK: - MKMapViewDelegate
